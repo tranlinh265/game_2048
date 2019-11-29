b9b0b6e19d8b635063ac661a5e53adc584898e18
linhtm <tranlinh265@gmail.com>
2019-11-30 00:31:58 +0900

    add guesture detector

--- lib/game.dart
+++ lib/game.dart
@@ -10,7 +10,6 @@ class _GameScreenState extends State<GameScreen> {
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        
         Center(
           child: Container(
             margin: EdgeInsets.only(top: 100),
@@ -18,20 +17,33 @@ class _GameScreenState extends State<GameScreen> {
             height: 300,
             color: Colors.white,
             child: GestureDetector(
-              onHorizontalDragEnd: (DragEndDetails details){
-                if(details.velocity.pixelsPerSecond.dx > 0){
+              onHorizontalDragEnd: (DragEndDetails details) {
+                if (details.velocity.pixelsPerSecond.dx > 0) {
                   print("left to right");
-                } else if(details.velocity.pixelsPerSecond.dx < 0){
+                } else if (details.velocity.pixelsPerSecond.dx < 0) {
                   print("right to left");
                 }
               },
-              onVerticalDragEnd: (details){
-                if (details.velocity.pixelsPerSecond.dy >0){
+              onVerticalDragEnd: (details) {
+                if (details.velocity.pixelsPerSecond.dy > 0) {
                   print("btm to top");
-                } else if (details.velocity.pixelsPerSecond.dy < 0){
-                  print ("top to btm");
+                } else if (details.velocity.pixelsPerSecond.dy < 0) {
+                  print("top to btm");
                 }
               },
+              child: GridView.count(
+                crossAxisCount: 4,
+                crossAxisSpacing: 10.0,
+                physics: NeverScrollableScrollPhysics(),
+                children: List.generate(16, (index) {
+                  return Container(
+                      width: 20,
+                      height: 20,
+                      child: Center(
+                        child: Text('$index', style: TextStyle(fontSize: 30),),
+                      ));
+                }),
+              ),
             ),
           ),
         ),
